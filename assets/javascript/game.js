3c59e70ad950ae2ddc59b8e8b8150a769809149f
ragepanda <jwoltz13@gmail.com>
2018-03-13 15:04:26 -0400

    All view components finished, only JS left

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+
+var guessObject =[
+    {status: "Your Ember is strong, take care not to lose it Chosen Undead.",
+    picFile: "assets\images\hangman-5.jpg",
+    estusPic: "assets\images\estus-flask-5.png",
+    },
+
+    {status: "You have lost your ember, but the Undead curse and bonfire will see to it that you return, willingly or not.",
+    picFile: "assets\images\hangman-4.png",
+    estusPic: "assets\images\estus-flask-4.png",
+    },
+
+
+    {status: "You have fallen, and fallen again. Do not give into despair and lose sight of your cause, or you may just lose yourself in the process.",
+    picFile: "assets\images\hangman-3.png",
+    estusPic: "assets\images\estus-flask-3.png",
+    },
+
+    {status: "The light at the end of the tunnel is fading, along with your purpose. Why did you set out on this task in the first place? It's becoming harder and harder to remember.",
+    picFile: "assets\images\hangman-2.png",
+    estusPic: "assets\images\estus-flask-2.png",
+    },
+
+    {status: "There's so very little human left in you, any more of this torment, and you'll surely crack and go hollow.",
+    picFile: "assets\images\hangman-1.jpg",
+    estusPic: "assets\images\estus-flask-1.png",
+    },
+];
+console.log("Guess Object[0]: "+guessObject[0].status);
