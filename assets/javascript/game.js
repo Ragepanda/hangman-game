a722d7444ae66428359ed3ce9407a7bafcac64f0
ragepanda <jwoltz13@gmail.com>
2018-03-14 05:17:43 -0400

    UI updates and loss win handling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,32 +3,32 @@ var guessObject = [
 
     {
         status: "There's so very little human left in you, any more of this torment, and you'll surely crack and go hollow.",
-        picFile: "assets\images\hangman-1.jpg",
-        estusPic: "assets\images\estus-flask-1.png",
+        statusPic: "assets/images/hangman-1.jpg",
+        estusPic: "assets/images/estus-flask-1.png",
     },
 
     {
-        status: "The light at the end of the tunnel is fading, along with your purpose. Why did you set out on this task in the first place? It's becoming harder and harder to remember.",
-        picFile: "assets\images\hangman-2.png",
-        estusPic: "assets\images\estus-flask-2.png",
+        status: "With each death",
+        statusPic: "assets/images/hangman-2.png",
+        estusPic: "assets/images/estus-flask-2.png",
     },
 
     {
-        status: "You have fallen, and fallen again. Do not give into despair and lose sight of your cause, or you may just lose yourself in the process.",
-        picFile: "assets\images\hangman-3.png",
-        estusPic: "assets\images\estus-flask-3.png",
+        status: "You aren't sure how many times you've died now, the prospect of staying dead is becoming appealing.",
+        statusPic: "assets/images/hangman-3.png",
+        estusPic: "assets/images/estus-flask-3.png",
     },
 
     {
-        status: "You have lost your ember, but the Undead curse and bonfire will see to it that you return, willingly or not.",
-        picFile: "assets\images\hangman-4.png",
-        estusPic: "assets\images\estus-flask-4.png",
+        status: "You have died lost your ember, but the Undead Curse and Bonfire will see to it that you return, willingly or not.",
+        statusPic: "assets/images/hangman-4.png",
+        estusPic: "assets/images/estus-flask-4.png",
     },
 
     {
-        status: "Your Ember is strong, take care not to lose it Chosen Undead.",
-        picFile: "assets\images\hangman-5.jpg",
-        estusPic: "assets\images\estus-flask-5.png",
+        status: "Your ember is strong, take care not to lose it and become ashen.",
+        statusPic: "assets/images/hangman-5.jpg",
+        estusPic: "assets/images/estus-flask-5.png",
     },
 ];
 
@@ -36,53 +36,47 @@ var bossNames = ["ASYLUM DEMON", "BELL GARGOYLE", "CAPRA DEMON", "CEASELESS DISC
     "GREAT GREY WOLF SIF", "GWYN LORD OF CINDER", "ORNSTEIN AND SMOUGH", "MOONLIGHT BUTTERFLY", "NITO", "SEATH THE SCALELESS", "TAURUS DEMON", "STRAY DEMON"
 ];
 
-var guessesLeft = 5;
 var wins = 0;
 var losses = 0;
 var firstRun = true;
 
-var bossName = bossNames[5];
+var bossName = bossNames[Math.floor(Math.random() * bossNames.length)];
 var hangmanString = "";
 var missedGuesses = ["", "", "", "", ""];
 var guessedIncorrect = 0;
-var correctGuesses = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
-var guessedCorrect = 0;
+var correctLetters = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
+var correctGuesses = 0;
+var isWin;
+var hangmanLetters = document.getElementById("hangman-letters");
+var title = document.getElementById("title");
+var statusPic = document.getElementById("status-pic");
+var estusPic = document.getElementById("estus-pic");
+var missedLetters = document.getElementById("missed-letters");
+var statusText = document.getElementById("status-text");
+var estusText = document.getElementById("estus-text");
+var winText = document.getElementById("wins");
+var lossText = document.getElementById("losses");
 
 document.onkeyup = function (event) {
 
     var userInput = String.fromCharCode(event.which).toUpperCase();
     console.log("Clicked Input: " + userInput);
 
-    var hangmanLetters = document.getElementById("hangman-letters");
-    var title = document.getElementById("title");
-    var statusPic = document.getElementById("status-pic");
-    var estusPic = document.getElementById("estus-pic");
-    var missedLetters = document.getElementById("missed-letters");
-    var statusText = document.getElementById("status-text");
-    var estusText= document.getElementById("estus-text");
 
-    if (firstRun === true) {
+    if (firstRun === true) {                                     // A logic branch for handling the transition from Pressing a key to starting
         firstRun = false;
         title.innerHTML = "Hangman Edition";
 
-        // Create the intial hangman spaces
-        for (var i = 0; i < bossName.length; i++) {
-            var hangmanChar = bossName[i];
+        // Create the intial hangman spaces for hangman String
+        initializeHangmanText();
 
-            if (hangmanChar === ' ') {
-                hangmanString = hangmanString + "<br> ";
-            }
-            else {
-                hangmanString = hangmanString + "_ ";
-            }
-        }
         console.log(hangmanString);
-        hangmanLetters.innerHTML = "<h1>" + hangmanString + "</h1>";
+        hangmanLetters.innerHTML = hangmanString;
     }
 
-    else {
+    else {                                                        // Logic branch for all other key-up inputs
         if (missedGuesses.includes(userInput) ||
-            correctGuesses.includes(userInput)) {
+            correctLetters.includes(userInput)) {
             alert("You have already picked this letter");
         }
 
@@ -90,45 +84,72 @@ document.onkeyup = function (event) {
             alert("Spaces are invalid but won't count against you.");
         }
 
-        else if (bossName.includes(userInput)) {
+        else if (bossName.includes(userInput)) {                    // Logic branch for correct letter choice
             console.log("You guessed a correct letter");
-            correctGuesses[guessedCorrect] = userInput;
-            guessedCorrect++;
+            correctLetters[correctGuesses] = userInput;
+            correctGuesses++;
             hangmanString = formatHangman(bossName, userInput, hangmanString);
             console.log("New hangman string after correct guess: " + hangmanString);
-            hangmanLetters.innerHTML = "<h1>" + hangmanString + "</h1>";
+            hangmanLetters.innerHTML = hangmanString;
 
-            if(hangmanString.indexOf("_")===-1)         /// Win condition is that there are no "_" characters in the hangman string
-                console.log("Winner");
+            if (hangmanString.indexOf("_") === -1) {        /// Win condition is that there are no "_" characters in the hangman string
+                alert("Winner");
+                wins++;
+                isWin = true;
+                reset();
+            }
         }
 
         else {                                         // incorrect letter branch
-            alert("You guessed an incorrect letter");
-            missedGuesses[guessedIncorrect]=userInput;
+            missedGuesses[guessedIncorrect] = userInput;
             guessedIncorrect++;
 
-            var missedLetterString = "Missed Letters: "
-            for(var i=0; i<guessedIncorrect; i++){
-                missedLetterString =missedLetterString.concat(missedGuesses[i]+" ");
+            var missedLetterString = "Missed Letters: ";
+            for (var i = 0; i < guessedIncorrect; i++) {
+                missedLetterString = missedLetterString.concat(missedGuesses[i] + " ");
             }
 
-            missedLetters.innerHTML=missedLetterString;  
+            missedLetters.innerHTML = missedLetterString;
 
-            if(guessedIncorrect===5){                              
+            estusPic.setAttribute("src", guessObject[4 - guessedIncorrect].estusPic);
+            statusPic.setAttribute("src", guessObject[4 - guessedIncorrect].statusPic);
+            statusText.innerHTML = guessObject[4 - guessedIncorrect].status;
+            estusText.innerHTML = "Guestus Flasks Remaining: " + (4 - guessedIncorrect);
+
+            if (guessedIncorrect === 5) {
                 alert("You lose");
+                losses++;
+                isWin = false;
             }
-
-            estusPic.setAttribute("src", guessObject[4-guessedIncorrect]);
-            statusPic.setAttribute("src", guessObject[4-guessedIncorrect]);
-            statusText.innerHTML = guessObject[4-guessedIncorrect];
-            estusText.innerHTML = "Guestus Flasks Remaining: "+(5-guessedIncorrect);
-
         }
     }
 };
 
+function reset() {
+    bossName = bossNames[Math.floor(Math.random() * bossNames.length)];
+    hangmanString = "";
+    missedGuesses = ["", "", "", "", ""];
+    guessedIncorrect = 0;
+    correctLetters = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
+    correctGuesses = 0;
 
-function formatHangman(bossName, newChar, currentHangman) {
+    estusPic.setAttribute("src", guessObject[4].estusPic);
+    statusPic.setAttribute("src", guessObject[4].statusPic);
+    statusText.innerHTML = guessObject[4].status;
+    estusText.innerHTML = "Guestus Flasks Remaining: 4";
+    initializeHangmanText();
+
+    if (isWin)
+        winText.innerHTML = "Wins: " + wins;
+    else
+        lossText.innerHtml = "Loses: " + losses;
+
+    missedLetters.innerHtml = "Missed Letters: ";
+
+    hangmanLetters.innerHTML = hangmanString;
+}
+
+function formatHangman(bossName, newChar, currentHangman) { // Helper function for manipulating displayed hangman string.
     var space = 0;
     for (var i = 0; i < bossName.length; i++) {
         if (bossName[i] === " ") {
@@ -144,16 +165,31 @@ function formatHangman(bossName, newChar, currentHangman) {
     return currentHangman;
 }
 
+function initializeHangmanText() {                // Function that formats the hangmanString global variable into an appropriate set of spaces and underscores
+    for (var i = 0; i < bossName.length; i++) {
+        var hangmanChar = bossName[i];
+
+        if (hangmanChar === ' ') {
+            hangmanString = hangmanString + "<br> ";
+        }
+        else {
+            hangmanString = hangmanString + "_ ";
+        }
+    }
+}
+
 function setCharAt(str, index, chr) {
     if (index > str.length - 1) return str;
     return str.substr(0, index) + chr + str.substr(index + 1);
 }
 
-function checkWin(hangmanString) {
-    if (hangmanString.indexOf("_")===-1) {
+function checkWin(hangmanString) {                   // Win condition is that there are no underscores left on the hangman board
+    if (hangmanString.indexOf("_") === -1) {
         return true;
     }
     return false;
 }
+
+
 
 //function hangmanTransition(statusPic, estusPic, statusText, estusText)
